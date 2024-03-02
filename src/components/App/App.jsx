0d790160577cd60080e55e3885637d2f96f31c7d
Oleksandr <saniafan1@gmail.com>
2024-03-02 21:51:03 +0200

    first components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from 'react'
+import SearchBar from '../SearchBar/SearchBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState("");
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar value={search} onSearch={setSearch}/>
     </>
   )
 }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import { Field, Form, Formik } from "formik";
+import { IoSearchSharp } from "react-icons/io5";
+
+import css from "./SearchBar.module.css";
+
+export default function SearchBar ({value, onSearch}) {
+    return (
+        <header className={css.header}>
+            <Formik initialValues={{value}} onSubmit={() => {onSearch}}>
+                <Form>
+                    <button className={css.button} type="submit"><IoSearchSharp/></button>
+                    <Field name="search"></Field>
+                </Form>
+            </Formik>
+        </header>
+    );
+}
